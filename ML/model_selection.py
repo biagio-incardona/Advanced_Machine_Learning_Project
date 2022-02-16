aeee5d877b5eedba54f654a17256f68a26ebd973
Biagio Incardona <biagio.incardona@outlook.it>
2022-02-16 19:55:59 +0100

    Update model_selection.py

--- ML/model_selection.py
+++ ML/model_selection.py
@@ -83,17 +83,20 @@ def model_evaluate(model, X, Y):
     plt.title("Confusion Matrix", fontdict={'size': 18}, pad=20)
 
 def save_results(search, file_name):
-  search = search.cv_results_
-  for i in range(len(search['params'])):
-    model = search['params'][i]
-    model['model']=str(model['model'])
-    model['model'] = model['model'][:model['model'].find('(')]
-    model['mean_test_score'] = search['mean_test_score'][i]
-    model['mean_score_time'] = search['mean_score_time'][i]
-    model['mean_fit_time'] = search['mean_fit_time'][i]
-    model["mean_train_score"] = search['mean_train_score'][i]
-    with open("{BaseDir}/Advanced_Machine_Learning_Project/ML/{FileName}.json".format(BaseDir=BASE_DIR, FileName=file_name),'a') as f:
-      json.dump(model, f,indent=4)
+    search = search.cv_results_
+    for i in range(len(search['params'])):
+        model = search['params'][i]
+        model['model'] = str(model['model'])
+        model['model'] = model['model'][:model['model'].find('(')]
+        model['mean_test_score'] = search['mean_test_score'][i]
+        model['mean_score_time'] = search['mean_score_time'][i]
+        model['mean_fit_time'] = search['mean_fit_time'][i]
+        model["mean_train_score"] = search['mean_train_score'][i]
+        with open("{BaseDir}/Advanced_Machine_Learning_Project/ML/{FileName}.json".format(BaseDir=BASE_DIR,
+                                                                                          FileName=file_name),
+                  'a') as f:
+            json.dump(model, f, indent=4)
+
 
 # must be removed when sending the project
 def apply_random_search(pip, params_grid, X_train, Y_train, results_file_name):
@@ -103,7 +106,46 @@ def apply_random_search(pip, params_grid, X_train, Y_train, results_file_name):
     search = trys.fit(X_train, Y_train)
     save_results(search, results_file_name)
 
+<<<<<<< HEAD
 def first_screening(df):
+=======
+
+def main():
+    path = "{BaseDir}/Advanced_Machine_Learning_Project/data/dataset.csv".format(BaseDir=BASE_DIR)
+    columns = ["sentiment", "ids", "date", "flag", "user", "text"]
+    final_columns = ["text", "sentiment"]
+
+    # explicit negations
+    negations = {"isn't": "is not", "aren't": "are not", "wasn't": "was not", "weren't": "were not",
+                 "haven't": "have not", "hasn't": "has not", "hadn't": "had not", "won't": "will not",
+                 "wouldn't": "would not", "don't": "do not", "doesn't": "does not", "didn't": "did not",
+                 "can't": "can not", "couldn't": "could not", "shouldn't": "should not", "mightn't": "might not",
+                 "mustn't": "must not"}
+
+    # convert twitter emojis in twitch style emojis
+    emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad',
+              ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+              ':-@': 'shocked', ':@': 'shocked', ':-$': 'confused',
+              ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+              '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+              '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink',
+              ';-)': 'wink', 'O:-)': 'angel', 'O*-)': 'angel', '(:-D': 'gossip', '=^.^=': 'cat'}
+
+    regex_subs = {
+        r"https?://[^s]+": "URL",  # replace any url with URL
+        "www.[^ ]+": "URL",  # replace any url with URL
+        r"@[^\s]+": "USR",  # replace any user tag with USR (the tag system is the same also in twitch)
+        r"(.)\1\1+": r"\1\1",  # replace 3 consecutive chars with 2
+        r"[\s]+": " ",  # remove consec spaces
+        "#[a-z0-9]*": ""  # remove hashtags, they are not used in twitch chats
+    }
+
+    sbStem = SnowballStemmer("english", True)
+    preprocess = ps.Preprocess(negations, emojis, regex_subs, sbStem)
+    df = load_dataset(path, columns, final_columns)
+    df = resize(df, 50000, "sentiment", 4)
+    df = preprocess.df_pre_process(df, "text", "sentiment")
+>>>>>>> 802ac085bc28314ba18cc89c7d86689fd9682455
     X_train, X_test, Y_train, Y_test = df_train_test_split(df, "text", "sentiment", test_size=0.05)
 
     w2v1 = w2v.W2VDecisionTreeClassifier()
