6dbc23e6cbce930775dbf8ec081fba1b8fc615ab
Biagio Incardona <biagio.incardona@outlook.it>
2022-02-19 12:51:06 +0100

    Update STClustering.py

--- ML/STClustering.py
+++ ML/STClustering.py
@@ -4,11 +4,12 @@ from scipy.spatial import distance
 from nltk.util import ngrams
 
 class STClustering:
-    def __init__(self, ngram_range=(1,1), max_features = 1000, r=1, lambda_=0.01):
+    def __init__(self, ngram_range=(1,1), max_features = 1000, r=0.9, lambda_=0.01, gap_time=1):
         self._ngram_range = ngram_range
         self._max_features = max_features
         self._r = r
         self._lambda = lambda_
+        self._gap_time = gap_time
         self._MC = [[{'bella': 2, 'francescano':1, 'de zio': 2, 'bella de': 2, 'zio': 2, 'de': 2, 'fratelli bella': 1, 'zio fratelli': 2, 'fratelli': 2},1,1]]
 
     def _tokenize(self, text):
@@ -108,23 +109,50 @@ class STClustering:
         return 1 - distance.cosine(mc_tfidf, message_tfidf)
 
     def _merge(self, micro_cluster, macro_cluster):
-        decay = 2**(-1*self._lambda*(micro_cluster[2]-macro_cluster[2]))
         for key in micro_cluster[0].keys():
             macro_cluster[0][key] += micro_cluster[0][key]
-        macro_cluster[1] = macro_cluster[1]*decay
+        macro_cluster[1] += micro_cluster[1]
         macro_cluster[2] = micro_cluster[2]
+
+    def _get_cosine_similarities(self, ngrams):
+        similarities = [0 for i in range(len(self._MC))]
+        for i in range(len(self._MC)):
+            similarities[i] = self._cosineSimilarity(self._MC[i][0], ngrams)
+        return similarities
+
+    def _update_weights(self, cur_time):
+        for cluster in self._MC:
+            decay = 2 ** (-1 * self._lambda * (cur_time - cluster[2]))
+            for key in cluster[0].keys():
+                cluster[0][key] = cluster[0][key]*decay
+            cluster[1] = cluster[1]*decay
+            cluster[2] = cur_time
+
+    def _cleanup(self):
+        pass
 
     def insert(self, message, time):
         ngrams = self._ngram_tokenizer(message)
         c = [ngrams, 1, time]
-        similarities = [0 for i in range(len(self._MC))]
-        for i in range(len(self._MC)):
-            similarities[i] = self._cosineSimilarity(self._MC[i][0], ngrams)
-        d = similarities.index(min(similarities))
-        if similarities[d] <= self._r:
-            self._MC[d] = self._merge(c, self._MC[d])
+        similarities = self._get_cosine_similarities(ngrams)
+        d = similarities.index(max(similarities))
+        if similarities[d] >= self._r:
+            self._merge(c, self._MC[d])
         else:
             self._MC.append(c)
+        self._update_weights(time)
+        if time - self._gap_time == 0:
+            print("gap")
+            self._cleanup()
+        print(len(self._MC))
         print(similarities)
-p = STClustering(ngram_range=(1,2))
-print(p.insert("bella", 1))
+
+p =STClustering(ngram_range=(1,2))
+p.insert("che persona del cavolo ma dai e che cazzo", 1.3)
+p.insert("sei una schifo allora gne gne gne fascistissimo", 1.5)
+p.insert("che persona del cavolo ma dai e che cazzo", 1.7)
+p.insert("sei una schifo allora gne gne gne fascistissimo", 1.9)
+p.insert("che persona del cavolo ma dai e che cazzo", 2.1)
+p.insert("sei una schifo allora gne gne gne fascistissimo", 2.3)
+p.insert("casiejw auasdj ldjiwqe cazzo", 2.5)
+p.insert("sei una schifo allora gne gne gne fascistissimo", 2.9)
